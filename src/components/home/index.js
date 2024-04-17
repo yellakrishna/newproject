2f6b638391f4fdd53a3ad7d3b58f6715c5dc359c
yellakrihsna <115071795+yellakrishna@users.noreply.github.com>
2024-04-17 13:21:17 +0530

    Update index.js

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const Home = () => {
   return (
     <div className='home-container'>
-      <img src='./images/fish5.jpg' className='home-back' alt= 'background'/>
+      <img src='/images/fish5.jpg' className='home-back' alt= 'background'/>
       <div className='main-heading'>
       <h1 >Online Fish Marketing</h1>
       
@@ -15,7 +15,7 @@ const Home = () => {
      
       </div>
       <div className='side-image'>
-        <img src='../images/side-image.jpeg' alt='fish'  className='image-side' />
+        <img src='/images/side-image.jpeg' alt='fish'  className='image-side' />
         
       </div>
       <button className='button'>Booking</button>
